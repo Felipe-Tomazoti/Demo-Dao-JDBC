33496faee07cb67bfa9481cda501dd50aa367995
Felipe-Tomazoti <felipecesar005@gmail.com>
2024-04-04 09:54:58 -0300

    SellerDaoJDBC and DaoFactory

--- demo-dao-jdbc/src/application/Main.java
+++ demo-dao-jdbc/src/application/Main.java
@@ -1,10 +1,19 @@
 package application;
 
+import dao.DaoFactory;
+import dao.SellerDao;
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, new Date(), "bob", "bob@gmail.com", 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println(seller);
     }
 }
